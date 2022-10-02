905f233634927b16c032418c6145025a0016ac16
nateachino <natealleyne88@gmail.com>
2022-10-01 22:25:43 -0400

    finished drawing and grid size changing

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 const container = document.getElementById("container");
+let value = 16;
+let formula = value * value;
 
 function createDivs(num) {
   for (i = 0; i < num; i++) {
     let newDiv = document.createElement("div");
     newDiv.classList.add("sketch");
     container.appendChild(newDiv);
+    newDiv.addEventListener("mouseover", function () {
+      colorBrick(this);
+    });
   }
 }
 
-function colorBrick(e) {
-  e.style.backgroundColor = "red";
+function deleteDivs() {
+  container.textContent = "";
 }
 
-createDivs(256);
+function colorBrick(e) {
+  e.style.backgroundColor = "black";
+}
 
-let divs = document.querySelectorAll(".sketch");
+function changeValue(e) {
+  value = e.target.value;
+  formula = value * value;
+  console.log(value);
+  inputValue.textContent = e.target.value + "x" + e.target.value;
+  deleteDivs();
+  console.log(formula);
+  createDivs(formula);
+  container.style.gridTemplateColumns = "repeat(" + value + ",auto)";
+  container.style.gridTemplateRows = "repeat(" + value + ",auto)";
+}
 
-divs.forEach((element) => {
-  element.addEventListener("mouseover", function () {
-    colorBrick(element);
-  });
+createDivs(formula);
+
+const slider = document.querySelector("input");
+const inputValue = document.getElementById("rangeValue");
+
+container.style.gridTemplateColumns = "repeat(" + value + ",auto)";
+container.style.gridTemplateRows = "repeat(" + value + ",auto)";
+
+slider.addEventListener("change", changeValue);
+
+const reset = document.querySelector("button");
+const sketch = document.querySelectorAll(".sketch");
+
+reset.addEventListener("mousedown", function (e) {
+  deleteDivs();
+  console.log(formula);
+  createDivs(formula);
 });
-
-container.style.gridTemplateColumns = "repeat(" + 16 + ",auto)";
-container.style.gridTemplateRows = "repeat(" + 16 + ",auto)";
